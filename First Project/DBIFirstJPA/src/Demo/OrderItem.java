c3a66fa7627975b98076a4ce0c28648f9b9e0089
Lukas Gaisbauer <gaisbauer.lukas@gmx.at>
2019-12-05 22:17:41 +0100

    Added Fields for OrderItem

--- First Project/DBIFirstJPA/src/Demo/OrderItem.java
+++ First Project/DBIFirstJPA/src/Demo/OrderItem.java
@@ -1,4 +1,21 @@
 package Demo;
 
+import com.sun.istack.NotNull;
+
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
 public class OrderItem {
+    @Id
+    @ManyToOne
+    @JoinColumn(name="orderID",insertable = false, updatable = false)
+    private int orderID;
+    @Id
+    @OneToOne
+    @JoinColumn(name="productId",insertable = false, updatable = false)
+    private int productId;
+    @NotNull
+    private int amount;
 }
