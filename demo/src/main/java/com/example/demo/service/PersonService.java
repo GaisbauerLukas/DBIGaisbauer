60e028ad1614ed1b3cff98694094b09ff866327a
Lukas Gaisbauer <gaisbauer.lukas@gmx.at>
2019-12-06 09:57:07 +0100

    Added getAllPeople Method

--- demo/src/main/java/com/example/demo/service/PersonService.java
+++ demo/src/main/java/com/example/demo/service/PersonService.java
@@ -6,10 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import java.util.LinkedList;
+import java.util.List;
+
 @Service
 public class PersonService {
 
     private final PersonDao personDao;
+    private List<Person> personList = new LinkedList<>();
 
     @Autowired
     public PersonService(@Qualifier("personDao") PersonDao personDao) {
@@ -17,6 +21,10 @@ public class PersonService {
     }
 
     public int  addPerson(Person person){
+        personList.add(person);
         return personDao.insertPerson(person);
+    }
+    public List<Person> getAllPeople() {
+        return personList;
     }
 }
